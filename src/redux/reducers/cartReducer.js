b7a2e55eaaf50580a208558b0150ac4ff7ca30b4
Erick <erisrnb18@gmail.com>
2021-10-05 20:40:37 -0400

    cart reducer built for redux

---
+++ src/redux/reducers/cartReducer.js
@@ -0,0 +1,36 @@
+export default function cartReducer(state = {
+    cart: [],
+    items: [],
+    item: [],
+    loading: true
+  }, action) {
+    switch (action.type) {
+      case 'LOADING':
+        return {
+          ...state,
+          cart: [],
+          items: [],
+          loading: true
+        }
+      case 'GET_CART':
+        return {
+          ...state, 
+          items: action.payload,
+          loading: false
+        }
+      case 'ADD_ITEM_TO_CART':
+        return {
+          ...state,
+          items: [...state.items, action.payload],
+          loading: false
+        }
+      case 'REMOVE_ITEM_FROM_CART':
+        return {
+          ...state,
+          items: action.payload,
+          loading: false
+        }
+      default:
+        return state;
+    }
+  }
